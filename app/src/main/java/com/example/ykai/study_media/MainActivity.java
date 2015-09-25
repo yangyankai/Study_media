d9a0b260ee873af4838736309514ba583fede131
ykai <yangyankai_mr@outlook.com>
2015-09-25 16:10:54 +0800

    media

---
+++ app/src/main/java/com/example/ykai/study_media/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.ykai.study_media;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+//import android.view.Button;
+
+
+
+
+
+public class MainActivity extends ActionBarActivity {
+    private Button btnPause, btnPlayUrl, btnStop;
+    private SeekBar skbProgress;
+    private Play player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle("在线音乐播放---hellogv编写");
+
+        btnPlayUrl = (Button) this.findViewById(R.id.btnPlayUrl);
+        btnPlayUrl.setOnClickListener(new ClickEvent());
+
+        btnPause = (Button) this.findViewById(R.id.btnPause);
+        btnPause.setOnClickListener(new ClickEvent());
+
+        btnStop = (Button) this.findViewById(R.id.btnStop);
+        btnStop.setOnClickListener(new ClickEvent());
+
+        skbProgress = (SeekBar) this.findViewById(R.id.skbProgress);
+        skbProgress.setOnSeekBarChangeListener(new SeekBarChangeEvent());
+        player = new Play(skbProgress);
+
+    }
+
+    class ClickEvent implements OnClickListener {
+
+        @Override
+        public void onClick(View arg0) {
+            if (arg0 == btnPause) {
+                player.pause();
+            } else if (arg0 == btnPlayUrl) {
+                //在百度MP3里随便搜索到的,大家可以试试别的链接
+                String url="http://mp3new.9ku.com/new/2015/01-28/658399.mp3";
+
+                player.playUrl(url);
+                Toast.makeText(getApplicationContext(), "默认Toast样式",Toast.LENGTH_SHORT).show();
+            } else if (arg0 == btnStop) {
+
+                player.stop();
+            }
+        }
+    }
+
+    class SeekBarChangeEvent implements SeekBar.OnSeekBarChangeListener {
+        int progress;
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+            // 原本是(progress/seekBar.getMax())*player.mediaPlayer.getDuration()
+            this.progress = progress * player.mediaPlayer.getDuration()
+                    / seekBar.getMax();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // seekTo()的参数是相对与影片时间的数字，而不是与seekBar.getMax()相对的数字
+            player.mediaPlayer.seekTo(progress);
+        }
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
